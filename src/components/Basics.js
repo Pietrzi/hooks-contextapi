93b32d0d955627849817d13aaf3099cb06a24db3
Pietrzi <pietrzi@op.pl>
2020-01-21 19:25:09 +0100

    form ok but checkbox inputs to change

--- src/components/Basics.js
+++ src/components/Basics.js
@@ -9,16 +9,55 @@ const Basics = () => {
             {name: 'Gdynia', id: uuid}
         ]);
 
+        const [description, setDescription] = useState('');
+
+        const [weather, setWeather] = useState('');
+
+        const [atractions, setAtractions] = useState({
+            circus: false,
+            church: false,
+            library: false
+        })
+
+        const [museums, setMuseums] = useState(0);
+
+
     const addCity = name => {
         setCities([...cities, {name, id: uuid}]);
     };
+
+    const addDescription = description => {
+        setDescription(description);
+    }
+
+    const chooseWeather = weather => {
+        setWeather(weather);
+    }
+
+    const pickAtractions = (circus, church, library) => {
+        setAtractions({
+            circus: circus,
+            church: church,
+            library: library
+        })
+    }
+
+    const museumsNumber = museums => {
+        setMuseums(museums);
+    }
 
     return (
         <div>
             <ul>
                 {cities.map(city => <li key={city.id}>{city.name}</li>)}
             </ul>
-            <NewCity addCity={addCity} />
+            {description ? <p>{description}</p> : null }
+            {weather ? <p>{weather}</p> : null }
+            {museums ? <p>{museums}</p> : null }
+            {atractions.circus ? <span>{atractions.circus}</span> : null }
+            {atractions.church ? <span>{atractions.church}</span> : null }
+            {atractions.library ? <span>{atractions.library}</span> : null }
+            <NewCity addCity={addCity} addDescription={addDescription} chooseWeather={chooseWeather} museumsNumber={museumsNumber} pickAtractions={pickAtractions} />
         </div>
     )
 }
--- src/components/NewCity.js
+++ src/components/NewCity.js
@@ -1,21 +1,66 @@
 import React, { useState } from 'react'
 
-const NewCity = ({addCity}) => {
+const NewCity = ({addCity, addDescription, chooseWeather, museumsNumber, pickAtractions}) => {
 
     const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [weather, setWeather] = useState('');
+    const [museums, setMuseums] = useState('');
+    const [atractions, setAtractions] = useState({
+        circus: false,
+        church: false,
+        library: false
+    })
 
     const handleSubmit = e => {
         e.preventDefault();
         addCity(name);
+        addDescription(description);
+        chooseWeather(weather);
+        museumsNumber(museums);
+        pickAtractions(atractions.circus, atractions.church, atractions.library);
         setName('');
+        setDescription('')
       }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <label>City name:</label>
-                <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                <div>
+                    <label>City name:</label>
+                    <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <label>Description:</label>
+                    <textarea value={description} onChange={e => setDescription(e.target.value)}></textarea>
+                </div>
+                <div>
+                    <label>What is the weather?</label>
+                    <select value={weather} onChange={e => setWeather(e.target.value)}>
+                        <option value="Sun">Sun</option>
+                        <option value="Cloudy">Cloudy</option>
+                        <option value="Rain">Rain</option>
+                        <option value="Windy">Windy</option>
+                    </select>
+                </div>
+                <div>
+                    <label>How many museums</label>
+                    <input type="number" value={museums} onChange={e => setMuseums(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Is there Circus?</label>
+                    <input name="Circus" type="checkbox" checked={atractions.circus} onChange={e => setAtractions(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Is there Church?</label>
+                    <input name="Church" type="checkbox" checked={atractions.church} onChange={e => setAtractions(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Is there Library?</label>
+                    <input name="Library" type="checkbox" checked={atractions.library} onChange={e => setAtractions(e.target.value)}></input>
+                </div>
                 <input type='submit' value='add' />
+                
             </form>
         </div>
     )
